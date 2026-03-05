d33de3e890097b5ff0282e5c7235213a76f6bb3f
dang <dang.nguyen14082002@gmail.com>
2026-03-05 22:00:17 +0700

    Loop around exit syscall

--- lib/_exit.c
+++ lib/_exit.c
@@ -3,11 +3,14 @@
 
 __attribute__((noreturn)) void _exit(int status_code)
 {
-    __asm__ volatile(
-        "int 0x80"
-        :
-        : "a" (SYS_EXIT),
-          "b" (status_code)
-        : "memory"
-    );
+    for (;;) 
+    {
+        __asm__ volatile(
+            "int 0x80"
+            :
+            : "a" (SYS_EXIT),
+              "b" (status_code)
+            : "memory"
+        );
+    }
 }
