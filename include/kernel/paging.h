a2e4c527040d42ef027d44cd9707c1754ccdd93b
dang <dang.nguyen14082002@gmail.com>
2026-03-15 19:08:19 +0700

    Implement user process paging management

--- include/kernel/paging.h
+++ include/kernel/paging.h
@@ -1,13 +1,36 @@
 #ifndef PAGING_H
 #define PAGING_H
 
-// #define PAGE_DIRECTORY_ENTRIES 1024
-// #define PAGE_TABLE_ENTRIES 1024
-// #define PAGE_SIZE 4096
-// #define PAGE_FLAG_PRESENT 0x1
-// #define PAGE_FLAG_WRITABLE 0x2
-// #define PAGE_FLAG_USER 0x4
+#include <stddef.h>
+#include <stdint.h>
+#include <kernel/memory.h>
 
-// uint8_t enable_paging(void);
+#define PAGING_DIRECTORY_POOL_SIZE 16
+#define PAGING_TABLE_POOL_SIZE 64
+#define PAGE_DIRECTORY_ENTRIES 1024
+#define PAGE_TABLE_ENTRIES 1024
+#define PAGE_SIZE 4096
+#define KERNEL_VMA_TO_LMA_OFFSET 0xc0000000
+#define PAGE_FLAG_PRESENT 0x1
+#define PAGE_FLAG_WRITABLE 0x2
+#define PAGE_FLAG_USER 0x4
+#define PAGE_FLAG_WRITE_THROUGH 0x8
+#define PAGE_FLAG_CACHE_DISABLE 0x10
+#define PAGE_FLAG_ACCESSED 0x20
+#define PAGE_FLAG_DIRTY 0x40
+#define PAGE_FLAG_GLOBAL 0x100
+#define PAGE_ADDRESS_MASK 0xfffff000
+#define PAGE_FLAGS_MASK 0xfff
+
+uintptr_t *alloc_pd(void);
+uint8_t free_pd(uintptr_t *pgd);
+uintptr_t *alloc_pt(void);
+uint8_t free_pt(uintptr_t *pgt);
+uintptr_t get_cr3(void);
+void set_cr3(uintptr_t cr3);
+uintptr_t get_pd_lma(uintptr_t pgd_virt);
+uintptr_t get_pd_vma(uintptr_t pgd_phys);
+uint8_t map_page(uintptr_t *pgd, uintptr_t virt_addr, uintptr_t phys_addr, uint32_t flags);
+uint8_t unmap_page(uintptr_t *pgd, uintptr_t virt_addr);
 
 #endif
--- kernel/paging.c
+++ kernel/paging.c
@@ -1,63 +1,144 @@
-// #include <stdint.h>
-// #include <kernel/paging.h>
-// #include <kernel/console.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <kernel/paging.h>
 
-// static uintptr_t page_directory[PAGE_DIRECTORY_ENTRIES] __attribute__((aligned(PAGE_SIZE)));
-// static uintptr_t kernel_page_table[PAGE_TABLE_ENTRIES] __attribute__((aligned(PAGE_SIZE)));
-// static uintptr_t user_page_table[PAGE_TABLE_ENTRIES] __attribute__((aligned(PAGE_SIZE)));
+static uintptr_t pd_pool[PAGING_DIRECTORY_POOL_SIZE][PAGE_DIRECTORY_ENTRIES] __attribute__((aligned(PAGE_SIZE)));
+static uint8_t pd_in_use[PAGING_DIRECTORY_POOL_SIZE];
+static uintptr_t pt_pool[PAGING_TABLE_POOL_SIZE][PAGE_TABLE_ENTRIES] __attribute__((aligned(PAGE_SIZE)));
+static uint8_t pt_in_use[PAGING_TABLE_POOL_SIZE];
 
-// static void clear_page_directory(void)
-// {
-//     for (uint32_t i = 0; i < PAGE_DIRECTORY_ENTRIES; i++)
-//     {
-//         page_directory[i] = 0;
-//     }
-// }
+uintptr_t *alloc_pd(void)
+{
+    for (uint32_t i = 0; i < PAGING_DIRECTORY_POOL_SIZE; i++)
+    {
+        if (!pd_in_use[i])
+        {
+            pd_in_use[i] = 1;
+            memset(pd_pool[i], 0, PAGE_SIZE);
+            return pd_pool[i];
+        }
+    }
+    return 0;
+}
 
-// static void setup_kernel_first_page_identity_map(void)
-// {
-//     for (uint32_t i = 0; i < PAGE_TABLE_ENTRIES; i++)
-//     {
-//         kernel_page_table[i] = (i * PAGE_SIZE) | PAGE_FLAG_PRESENT | PAGE_FLAG_WRITABLE;
-//     }
-//     page_directory[0] = ((uint32_t) kernel_page_table) | PAGE_FLAG_PRESENT | PAGE_FLAG_WRITABLE;
-// }
+uint8_t free_pd(uintptr_t *pgd)
+{
+    for (uint32_t i = 0; i < PAGING_DIRECTORY_POOL_SIZE; i++)
+    {
+        if (pd_pool[i] == pgd)
+        {
+            pd_in_use[i] = 0;
+            return 1;
+        }
+    }
+    return 0;
+}
 
-// static void setup_user_first_page(void)
-// {
-//     for (uint32_t i = 0; i < PAGE_TABLE_ENTRIES; i++)
-//     {
-//         user_page_table[i] = (0x400000 + (i * PAGE_SIZE)) | PAGE_FLAG_PRESENT | PAGE_FLAG_WRITABLE | PAGE_FLAG_USER;
-//     }
-//     page_directory[1] = ((uint32_t) user_page_table) | PAGE_FLAG_PRESENT | PAGE_FLAG_WRITABLE | PAGE_FLAG_USER;
-// }
+uintptr_t *alloc_pt(void)
+{
+    for (uint32_t i = 0; i < PAGING_TABLE_POOL_SIZE; i++)
+    {
+        if (!pt_in_use[i])
+        {
+            pt_in_use[i] = 1;
+            memset(pt_pool[i], 0, PAGE_SIZE);
+            return pt_pool[i];
+        }
+    }
+    return 0;
+}
 
-// uint8_t enable_paging(void)
-// {
-//     console_puts("Enabling memory paging ... ");
-//     clear_page_directory();
-//     setup_kernel_first_page_identity_map();
-//     setup_user_first_page();
-//     __asm__ volatile(
-//         "mov cr3, %0"
-//         :
-//         : "r"(page_directory)
-//         : "memory"
-//     );
-//     uint32_t cr0;
-//     __asm__ volatile(
-//         "mov %0, cr0"
-//         : "=r"(cr0)
-//         :
-//         :
-//     );
-//     cr0 |= 0x80000000;
-//     __asm__ volatile(
-//         "mov cr0, %0"
-//         :
-//         : "r"(cr0)
-//         : "memory"
-//     );
-//     console_puts("Done\n");
-//     return 1;
-// }
+uint8_t free_pt(uintptr_t *pgt)
+{
+    for (uint32_t i = 0; i < PAGING_TABLE_POOL_SIZE; i++)
+    {
+        if (pt_pool[i] == pgt)
+        {
+            pt_in_use[i] = 0;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+uintptr_t get_cr3(void)
+{
+    uintptr_t cr3;
+    __asm__ volatile(
+        "mov %0, cr3\n"
+        : "=r"(cr3)
+        :
+        : "memory"
+    );
+    return cr3 & PAGE_ADDRESS_MASK;
+}
+
+void set_cr3(uintptr_t cr3)
+{
+    __asm__ volatile(
+        "mov cr3, %0\n"
+        :
+        : "r"(cr3)
+        : "memory"
+    );
+}
+
+uintptr_t get_pd_lma(uintptr_t pgd_virt)
+{
+    return (pgd_virt - KERNEL_VMA_TO_LMA_OFFSET) & PAGE_ADDRESS_MASK;
+}
+
+uintptr_t get_pd_vma(uintptr_t pgd_phys)
+{
+    return (pgd_phys & PAGE_ADDRESS_MASK) + KERNEL_VMA_TO_LMA_OFFSET;
+}
+
+static void flush_tlb_single(uintptr_t vma)
+{
+    __asm__ volatile(
+        "invlpg [%0]\n"
+        :
+        : "r"(vma)
+        : "memory"
+    );
+}
+
+uint8_t map_page(uintptr_t *pgd, uintptr_t page_base_addr, uintptr_t frame_base_addr, uint32_t flags)
+{
+    if (!(flags & PAGE_FLAG_PRESENT))
+    {
+        return 0;
+    }
+    uint16_t pde_index = (page_base_addr >> 22) & 0x3ff;
+    uint16_t pte_index = (page_base_addr >> 12) & 0x3ff;
+    uintptr_t *page_table = (uintptr_t *) get_pd_vma(pgd[pde_index]);
+    if (!page_table)
+    {
+        return 0;
+    }
+    page_table[pte_index] = frame_base_addr | flags;
+    if (get_cr3() == get_pd_lma((uintptr_t) pgd))
+    {
+        flush_tlb_single(page_base_addr);
+    }
+    return 1;
+}
+
+uint8_t unmap_page(uintptr_t *pgd, uintptr_t page_base_addr)
+{
+    uint16_t pde_index = (page_base_addr >> 22) & 0x3ff;
+    uint16_t pte_index = (page_base_addr >> 12) & 0x3ff;
+    uintptr_t pde = pgd[pde_index];
+    if (!(pde & PAGE_FLAG_PRESENT))
+    {
+        return 0;
+    }
+    uintptr_t *page_table = (uintptr_t *) get_pd_vma(pde);
+    page_table[pte_index] = 0;
+    if (get_cr3() == get_pd_lma((uintptr_t) pgd))
+    {
+        flush_tlb_single(page_base_addr);
+    }
+    return 1;
+}
